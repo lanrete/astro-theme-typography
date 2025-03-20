7adad02b640999ba1f2045c57dea7dc86f7bdd21
lanrete <jeorito@gmail.com>
2025-03-20 09:31:52 +0800

    Update user.ts

--- src/.config/user.ts
+++ src/.config/user.ts
@@ -2,6 +2,20 @@ import type { UserConfig } from '~/types'
 
 export const userConfig: Partial<UserConfig> = {
   // Override the default config here
-  // site: { title: "講評世界" },
+  site: {
+    title: 'Inner Side',
+    author: 'lrt',
+    description: 'Everyone has a inner side. This is mine.',
+    website: 'https://lrt.one/',
+    socialLinks: [
+      {
+        name: 'github',
+        href: 'https://github.com/lanrete/astro-theme-typography',
+      },
+      {
+        name: 'rss',
+        href: '/atom.xml',
+      }
+    },
   // seo: { twitter: "@moeyua13" },
 }
