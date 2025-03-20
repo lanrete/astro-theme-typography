1a4d6267fdb267f8c5205720184b914ea0e4ad97
lanrete <jeorito@gmail.com>
2025-03-20 09:41:06 +0800

    Update user.ts

--- src/.config/user.ts
+++ src/.config/user.ts
@@ -3,7 +3,7 @@ import type { UserConfig } from '~/types'
 export const userConfig: Partial<UserConfig> = {
   // Override the default config here
   site: {
-    title: 'Inner Side',
+    title: '内心世界',
     author: 'lrt',
     description: 'Everyone has a inner side. This is mine.',
     website: 'https://lrt.one/',
