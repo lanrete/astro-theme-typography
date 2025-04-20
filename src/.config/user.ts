01bd988f82ff89d5311a0cac84a12327ec667b5e
yifan mac <jeorito@gmail.com>
2025-04-20 13:05:21 +0800

    Update blog setting
    Update subtitle

--- src/.config/user.ts
+++ src/.config/user.ts
@@ -4,6 +4,7 @@ export const userConfig: Partial<UserConfig> = {
   // Override the default config here
   site: {
     title: '内心世界',
+    subtitle: 'Inner Space',
     author: 'lrt',
     description: 'Everyone has a inner side. This is mine.',
     website: 'https://lrt.one/',
